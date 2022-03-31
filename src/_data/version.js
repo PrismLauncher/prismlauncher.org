109c6339e97a1181808e6cf303f140c25181431e
Ezekiel Smith <ezekielsmith@protonmail.com>
2022-03-31 16:48:49 +1000

    Update version.js

--- src/_data/version.js
+++ src/_data/version.js
@@ -2,14 +2,14 @@ const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function () {
 	// https://developer.github.com/v3/repos/#get
-	let json = await EleventyFetch("https://api.github.com/repos/PolyMC/PolyMC/tags", {
+	let json = await EleventyFetch("https://api.github.com/repos/PolyMC/PolyMC/releases", {
 		duration: "1d", // 1 day
 		type: "json" // also supports "text" or "buffer"
 	});
 
 	const tags = []
 	for (i in json) {
-		tags.push(json[i].name)
+		tags.push(json[i].tag_name)
 	}
 
 	return {
