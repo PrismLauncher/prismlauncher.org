f3543ffaa19a1853b26625503b0acd165e89b656
Sefa Eyeoglu <contact@scrumplex.net>
2023-02-09 10:38:44 +0100

    chore: remove unused browsersync config
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- .eleventy.js
+++ .eleventy.js
@@ -165,24 +165,6 @@ module.exports = function (eleventyConfig) {
   }).use(markdownItTableOfContents, {"includeLevel": [1,2,3]});
   eleventyConfig.setLibrary("md", markdownLibrary);
 
-  // Override Browsersync defaults (used only with --serve)
-  eleventyConfig.setBrowserSyncConfig({
-    callbacks: {
-      ready: function (err, browserSync) {
-        const content_404 = fs.readFileSync("_site/404.html");
-
-        browserSync.addMiddleware("*", (req, res) => {
-          // Provides the 404 content without redirect.
-          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
-          res.write(content_404);
-          res.end();
-        });
-      },
-    },
-    ui: false,
-    ghostMode: false,
-  });
-
   return {
     // Control which files Eleventy will process
     // e.g.: *.md, *.njk, *.html, *.liquid
