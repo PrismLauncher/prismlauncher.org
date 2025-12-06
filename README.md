cea57b6988c4ecfbb574f647314f791048a8bce4
Sefa Eyeoglu <contact@scrumplex.net>
2025-12-06 13:01:38 +0100

    chore: format code
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- README.md
+++ README.md
@@ -13,13 +13,16 @@ root of this repository. You may use [`nix-direnv`][nix-direnv] by copying
 Install [pnpm][pnpm-install].
 
 clone repository:
+
 ```sh
 git clone https://github.com/PrismLauncher/prismlauncher.org.git
 ```
 
 run local build:
+
 ```sh
 pnpm run dev
 ```
+
 [pnpm-install]: https://pnpm.io/installation
 [nix-direnv]: https://github.com/nix-community/nix-direnv
--- astro.config.mjs
+++ astro.config.mjs
@@ -8,101 +8,101 @@ import mdx from "@astrojs/mdx";
 import starlight from "@astrojs/starlight";
 
 export default defineConfig({
-  site: process.env.DEPLOY_URL || "https://prismlauncher.org",
+	site: process.env.DEPLOY_URL || "https://prismlauncher.org",
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	vite: {
+		plugins: [tailwindcss()],
+	},
 
-  integrations: [
-    starlight({
-      title: "Prism Launcher Wiki",
+	integrations: [
+		starlight({
+			title: "Prism Launcher Wiki",
 
-      logo: {
-        light: "./public/img/logo-textLight.svg",
-        dark: "./public/img/logo-textDark.svg",
-        replacesTitle: true
-      },
+			logo: {
+				light: "./public/img/logo-textLight.svg",
+				dark: "./public/img/logo-textDark.svg",
+				replacesTitle: true,
+			},
 
-      // NOTE(@getchoo): required-ish to avoid our categories all being in one "Wiki" category
-      //
-      // Usually Starlight would handle this nicely, but since we want everything under a
-      // nice `/wiki` endpoint, this will do
-      //
-      // TODO(@ZekeZ)
-      // There is a better way to do this if I remember correctly and I will need to investigate further.
-      sidebar: [
-        {
-          label: "Overview",
-          autogenerate: {
-            directory: "wiki/overview",
-          },
-        },
-        {
-          label: "Getting Started",
-          autogenerate: {
-            directory: "wiki/getting-started",
-          },
-        },
-        {
-          label: "Help",
-          autogenerate: {
-            collapsed: true,
-            directory: "wiki/help",
-          },
-        },
-        "wiki/branding",
-        {
-          label: "Development",
-          autogenerate: {
-            directory: "wiki/development",
-          },
-        },
-      ],
+			// NOTE(@getchoo): required-ish to avoid our categories all being in one "Wiki" category
+			//
+			// Usually Starlight would handle this nicely, but since we want everything under a
+			// nice `/wiki` endpoint, this will do
+			//
+			// TODO(@ZekeZ)
+			// There is a better way to do this if I remember correctly and I will need to investigate further.
+			sidebar: [
+				{
+					label: "Overview",
+					autogenerate: {
+						directory: "wiki/overview",
+					},
+				},
+				{
+					label: "Getting Started",
+					autogenerate: {
+						directory: "wiki/getting-started",
+					},
+				},
+				{
+					label: "Help",
+					autogenerate: {
+						collapsed: true,
+						directory: "wiki/help",
+					},
+				},
+				"wiki/branding",
+				{
+					label: "Development",
+					autogenerate: {
+						directory: "wiki/development",
+					},
+				},
+			],
 
-      social: [
-        {
-          icon: "mastodon",
-          label: "Mastodon",
-          href: "https://floss.social/@PrismLauncher",
-        },
-        {
-          icon: "blueSky",
-          label: "Bluesky",
-          href: "https://bsky.app/profile/prismlauncher.org",
-        },
-        {
-          icon: "discord",
-          label: "Discord",
-          href: "https://discord.gg/ArX2nafFz2",
-        },
-        {
-          icon: "matrix",
-          label: "Matrix",
-          href: "https://matrix.to/#/#prismlauncher:matrix.org",
-        },
-        {
-          icon: "reddit",
-          label: "Reddit",
-          href: "https://www.reddit.com/r/PrismLauncher/",
-        },
-        {
-          icon: "github",
-          label: "GitHub",
-          href: "https://github.com/PrismLauncher/PrismLauncher",
-        },
-        {
-          icon: "email",
-          label: "E-mail",
-          href: "mailto:contact@prismlauncher.org",
-        },
-      ],
+			social: [
+				{
+					icon: "mastodon",
+					label: "Mastodon",
+					href: "https://floss.social/@PrismLauncher",
+				},
+				{
+					icon: "blueSky",
+					label: "Bluesky",
+					href: "https://bsky.app/profile/prismlauncher.org",
+				},
+				{
+					icon: "discord",
+					label: "Discord",
+					href: "https://discord.gg/ArX2nafFz2",
+				},
+				{
+					icon: "matrix",
+					label: "Matrix",
+					href: "https://matrix.to/#/#prismlauncher:matrix.org",
+				},
+				{
+					icon: "reddit",
+					label: "Reddit",
+					href: "https://www.reddit.com/r/PrismLauncher/",
+				},
+				{
+					icon: "github",
+					label: "GitHub",
+					href: "https://github.com/PrismLauncher/PrismLauncher",
+				},
+				{
+					icon: "email",
+					label: "E-mail",
+					href: "mailto:contact@prismlauncher.org",
+				},
+			],
 
-      // NOTE: This would conflict with our own 404
-      disable404Route: true,
-    }),
-    icon(),
-    sitemap(),
-    mdx(),
-  ],
+			// NOTE: This would conflict with our own 404
+			disable404Route: true,
+		}),
+		icon(),
+		sitemap(),
+		mdx(),
+	],
 });
