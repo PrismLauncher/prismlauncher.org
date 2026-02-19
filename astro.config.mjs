2ecbc787b1352820e844301f6dc981f6ed4bb353
MONGCHAW <59840759+notMONGCHAW@users.noreply.github.com>
2026-02-19 16:31:02 +0600

    Revert "modified astro config"
    This reverts commit c87d8347ce8736ebea90549c26c35e2be6a6227c.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,6 @@ import starlight from "@astrojs/starlight";
 
 export default defineConfig({
 	site: process.env.DEPLOY_URL || "https://prismlauncher.org",
-	trailingSlash: "never",
 	output: "static",
 	adapter: cloudflare(),
 
